81f42a650cb2f965f26d37cfb230c548cd050bb5
yashweblife <yashweblife20@gmail.com>
2023-01-17 22:32:10 -0600

    add controller class

---
+++ src/Controller.cpp
@@ -0,0 +1,42 @@
+#include<Arduino.h>
+#include<Controller.h>
+
+Controller::Controller(){}
+Controller::Controller(int val){
+    pin=val;
+}
+Controller::Controller(int val, int _min, int _max){
+    pin=val;
+    minVal=_min;
+    maxVal=_max;
+}
+void Controller::setPin(int val){
+    pin=val;
+}
+void Controller::setMin(float val){
+    minVal=val;
+}
+void Controller::setMax(float val){
+    maxVal=val;
+}
+float Controller::getValue(){
+    digitalWrite(pin,1);
+    float val=analogRead(A0);
+    digitalWrite(pin,0);
+    return(val);
+}
+void Controller::caliberateMin(){
+    float val = 0;
+    for(int i=0;i<100;i++){val+=getValue();}
+    val/=100;
+    minVal=val;
+}
+void Controller::caliberateMax(){
+    float val = 0;
+    for(int i=0;i<100;i++){val+=getValue();}
+    val/=100;
+    maxVal=val;
+}
+float Controller::mapValue(float _min, float _max){
+    return(map(getValue(),minVal,maxVal,_min,_max));
+}
---
+++ src/Controller.h
@@ -0,0 +1,21 @@
+#ifndef Controller_h
+#define Controller_h
+#include <Arduino.h>
+class Controller
+{
+public:
+    Controller();
+    Controller(int val);
+    Controller(int val, int _min, int _max);
+    int pin = 14;
+    float minVal = 0;
+    float maxVal = 1024;
+    void setPin(int val);
+    void setMin(float val);
+    void setMax(float val);
+    float getValue();
+    void caliberateMin();
+    void caliberateMax();
+    float mapValue(float _min, float _max);
+};
+#endif
