a2e7137dbe53e10155785109fecdc72ad52451e9
yashweblife <yashweblife20@gmail.com>
2023-01-17 22:42:46 -0600

    new test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 
 #include <OledHelper.h>
+#include<Controller.h>
+
 OledHelper oled;
+Controller lhs(14);
+Controller rhs(12);
+
 void setup() {
   Serial.begin(9600);
   if(!oled.setup_oled()){
     for(;;){}
   }
-  Serial.println("Oled Initialized");
   oled.clear();
-  for(int i=0;i<10;i++){
-    oled.circle(i*10,10,5,false);
-  }
-  oled.show();
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  oled.clear();
+  oled.text("LHS:  "+String(lhs.getValue())+"\nRHS:  "+String(rhs.getValue()));
+  oled.show();
 }
