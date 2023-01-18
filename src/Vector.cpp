ca35b89c460b752d286c61d228c30c23df8d16ee
yashweblife <yashweblife20@gmail.com>
2023-01-17 22:42:39 -0600

    remove duplicate

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -13,12 +13,6 @@ Vector::Vector(float x, float y)
     this->y = y;
     this->z = 0;
 }
-Vector::Vector(float x, float y)
-{
-    this->x = x;
-    this->y = y;
-    this->z = 0;
-}
 Vector::Vector(float x, float y, float z)
 {
     this->x = x;
