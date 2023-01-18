d4f7209d3dacb7f77ab4f2ee49bbab2ee0df3bd4
YashGiri0101 <yash@witblox.com>
2023-01-18 13:47:12 -0600

    add functions to ball

--- src/Ball.h
+++ src/Ball.h
@@ -14,13 +14,13 @@ public:
     int size;
 
     Ball();
-    Ball(int start_x = 64, int start_y = 32);
+    Ball(int start_x, int start_y);
     void setSize(int val);
     void setPos(int x, int y);
     void setVel(int x, int y);
     void setAcc(int x, int y);
     void reverseVel(bool comp=false);
-    void bound(int x = 0, int y = 0, int dx = 128, int dy = 64);
+    int bound(int x = 0, int y = 0, int dx = 128, int dy = 64);
     bool checkCollisionWithBall(Ball b);
     bool checkCollisionWithBox();
     void draw(OledHelper *oled);
