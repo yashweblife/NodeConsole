fc48c139618141a945f465b286bbc867657d6f05
yashweblife <yashweblife20@gmail.com>
2023-01-17 22:08:24 -0600

    add oled test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 
 #include <OledHelper.h>
+OledHelper oled;
 void setup() {
+  Serial.begin(9600);
+  if(!oled.setup_oled()){
+    for(;;){}
+  }
+  Serial.println("Oled Initialized");
+  oled.clear();
+  for(int i=0;i<10;i++){
+    oled.circle(i*10,10,5,false);
+  }
+  oled.show();
+  
 }
 
 void loop() {
