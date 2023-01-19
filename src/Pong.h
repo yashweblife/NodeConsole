b88c5945257cd624f44d8951e9fdfad5edad2319
yashweblife <yashweblife20@gmail.com>
2023-01-18 19:44:07 -0600

    major changes

--- src/Pong.h
+++ src/Pong.h
@@ -1,19 +1,21 @@
 #ifndef Pong_h
 #define Pong_h
+
 #include <Arduino.h>
-#include <Controller.h>
+#include<OledHelper.h>
+#include <Box.h>
 #include <Ball.h>
 class Pong
 {
 public:
-    Controller lhs;
-    Controller rhs;
+    Box lhs;
+    Box rhs;
     Ball b;
 
     bool gameState = false;
     Pong();
-    void draw();
-    void update();
+    void draw(OledHelper &oled);
+    void update(int p1_pos, int p2_pos);
 };
 
 #endif
