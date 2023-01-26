f1a3476bb2638e432a0490456f04f733b38d696e
yashweblife <yashweblife20@gmail.com>
2023-01-26 13:27:21 -0600

    finish basic sketch

--- src/Text.cpp
+++ src/Text.cpp
@@ -2,3 +2,23 @@
 #include <Text.h>
 #include <Vector.h>
 #include <OledHelper.h>
+
+Text::Text(String data)
+{
+    value = data;
+}
+int Text::getWidth() {}
+int Text::getHeight() {}
+void Text::setScale(int s) {}
+void Text::setPos(int x, int y) {}
+void Text::setValue(String data) {}
+void Text::calculatePadding() {}
+void Text::calculatePosition() {}
+void Text::centerCut() {}
+void Text::underline() {}
+void Text::setBorderLeft() {}
+void Text::setBorderRight() {}
+void Text::setBorderTop() {}
+void Text::setBorderBottom() {}
+void Text::setBorder() {}
+void Text::draw(OledHelper &oled) {}
