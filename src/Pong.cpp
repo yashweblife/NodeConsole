dc54c5cbf4f0312ecf42648b645443293a75fd52
yashweblife <yashweblife20@gmail.com>
2023-01-30 04:49:49 -0600

    Initial working code

--- src/Pong.cpp
+++ src/Pong.cpp
@@ -2,29 +2,81 @@
 #include <Pong.h>
 #include <Ball.h>
 #include <OledHelper.h>
+#include <Controller.h>
 
-Pong::Pong()
+
+void Pong::begin()
 {
-    b = Ball();
-    b.setVel(1, 1);
+    ball = Ball(64, 32);
+    player1 = Box(10, 20);
+    player2 = Box(10, 20);
+    lhs = Controller(14);
+    rhs = Controller(12);
+    game_mode = true;
+    winner = false;
+    ball.setPos(64,32);
+    ball.setVel(-1, -1);
+    player1.setPos(0, 32 - 10);
+    player2.setPos(128 - 10, 32 - 10);
 }
-void Pong::draw(OledHelper &oled)
+void Pong::game(OledHelper &oled)
 {
-    lhs.draw(oled);
-    rhs.draw(oled);
-    b.draw(oled);
-}
-void Pong::update(int p1_pos, int p2_pos)
-{
-    if (gameState == true)
+    for (int i = 0; i < 3; i++)
     {
-        b.update();
-        lhs.setPos(0, p1_pos);
-        rhs.setPos(128 - rhs.size.x, p2_pos);
-        b.bound(0, 0, 128, 64);
-        if (lhs.checkCollisionWithBall(b.pos.x, b.pos.y, b.radius) || rhs.checkCollisionWithBall(b.pos.x, b.pos.y, b.radius))
-        {
-            b.reverseVel(true);
-        }
+        oled.clear();
+        oled.text("Pong", 10, 10, 2, true);
+        oled.text(String(3 - i), 30, 35, 2, true);
+        delay(1000);
+        oled.show();
     }
+    oled.clear();
+    oled.text("Go", 10, 10, 2, true);
+    oled.show();
+    delay(1000);
+    while (game_mode == true)
+    {
+        oled.clear();
+
+        int p1 = map(rhs.getValue(), 137, 238, 0, 64 - player1.size.y);
+        int p2 = map(lhs.getValue(), 209, 6, 0, 64 - player2.size.y);
+
+        player1.setPos(player1.pos.x, p1);
+        player2.setPos(player2.pos.x, p2);
+
+        int bound_check = ball.bound();
+        ball.update();
+
+        if (player1.checkCollisionWithBall(ball.pos.x, ball.pos.y, ball.radius) || player2.checkCollisionWithBall(ball.pos.x, ball.pos.y, ball.radius))
+        {
+            ball.reverseVel(true);
+        }
+        if (bound_check == 1)
+        {
+            game_mode = false;
+            winner = true;
+        }
+        if (bound_check == 3)
+        {
+            game_mode = false;
+            winner = false;
+        }
+
+        player1.draw(oled);
+        player2.draw(oled);
+        ball.draw(oled);
+        oled.line(64, 0, 64, 64);
+        oled.show();
+    }
+    delay(500);
+    oled.clear();
+    if (winner)
+    {
+        oled.text("Player 2 Wins", 20, 20, 1.5, true);
+    }
+    else
+    {
+        oled.text("Player 1 Wins", 20, 20, 1.5, true);
+    }
+    oled.show();
+    delay(2000);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <Controller.h>
 #include <Ball.h>
 #include <Box.h>
+#include <Pong.h>
 OledHelper oled;
-Controller lhs(14);
-Controller rhs(12);
+
+
+Pong pong;
 
 static const unsigned char PROGMEM fighter[] =
     {0b00000010, 0b01000000,
@@ -35,85 +37,15 @@ void setup()
   }
   oled.clear();
   oled.rotate();
+  pong.game(oled);
 }
 
-void pong()
-{
-  bool game_mode = true;
-  bool winner = false;
-  Ball ball(64, 32);
-  ball.setSize(3);
-  ball.setVel(1, 1);
-  Box player1(10, 20);
-  player1.setPos(0, 32 - 10);
-
-  Box player2(10, 20);
-  player2.setPos(128 - 10, 32 - 10);
-  for (int i = 0; i < 3; i++)
-  {
-    oled.clear();
-    oled.text("Pong", 10, 10, 2, true);
-    oled.text(String(3 - i), 30, 35, 2, true);
-    delay(1000);
-    oled.show();
-  }
-  oled.clear();
-  oled.text("Go", 10, 10, 2, true);
-  oled.show();
-  delay(1000);
-  while (game_mode == true)
-  {
-    oled.clear();
-
-    int p1 = map(rhs.getValue(), 137, 238, 0, 64 - player1.size.y);
-    int p2 = map(lhs.getValue(), 209, 6, 0, 64 - player2.size.y);
-
-    player1.setPos(player1.pos.x, p1);
-    player2.setPos(player2.pos.x, p2);
-
-    int bound_check = ball.bound();
-    ball.update();
-
-    if (player1.checkCollisionWithBall(ball.pos.x, ball.pos.y, ball.radius) || player2.checkCollisionWithBall(ball.pos.x, ball.pos.y, ball.radius))
-    {
-      ball.reverseVel(true);
-    }
-    if (bound_check == 1)
-    {
-      game_mode = false;
-      winner = true;
-    }
-    if (bound_check == 3)
-    {
-      game_mode = false;
-      winner = false;
-    }
-
-    player1.draw(oled);
-    player2.draw(oled);
-    ball.draw(oled);
-    oled.line(64, 0, 64, 64);
-    oled.show();
-  }
-  delay(500);
-  oled.clear();
-  if (winner)
-  {
-    oled.text("Player 2 Wins", 20, 20, 1.5, true);
-  }
-  else
-  {
-    oled.text("Player 1 Wins", 20, 20, 1.5, true);
-  }
-  oled.show();
-  delay(2000);
-}
 
 void loop()
 {
-  int p1 = map(rhs.getValue(), 137, 238, 0, 128);
-  int p2 = map(lhs.getValue(), 209, 6, 0, 64);
-  oled.clear();
-  oled.bitmap(p1, p2, 16, 16, fighter);
-  oled.show();
+  // int p1 = map(rhs.getValue(), 137, 238, 0, 128);
+  // int p2 = map(lhs.getValue(), 209, 6, 0, 64);
+  // oled.clear();
+  // oled.bitmap(p1, p2, 16, 16, fighter);
+  // oled.show();
 }
