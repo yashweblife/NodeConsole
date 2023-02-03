b7bc1eca790b3e7952f6f36ff98dff01d967af34
yashweblife <yashweblife20@gmail.com>
2023-02-02 22:40:28 -0600

    Add code doc to text

--- src/Text.h
+++ src/Text.h
@@ -3,7 +3,9 @@
 #include <Arduino.h>
 #include <Vector.h>
 #include <OledHelper.h>
-
+/**
+ * @brief Text is a library to handle string dislplay over the oled
+ */
 class Text
 {
 public:
@@ -23,22 +25,96 @@ public:
     bool fill;
     bool underline;
     bool center_cut;
+    /**
+     * @brief Construct a new Text object
+     * 
+     * @param data 
+     */
     Text(String data);
+    /**
+     * @brief Get the Width of the text
+     * 
+     * @return int 
+     */
     int getWidth();
+    /**
+     * @brief Get the Height of the text
+     * 
+     * @return int 
+     */
     int getHeight();
+    /**
+     * @brief Set the Scale of the text
+     * 
+     * @param s 
+     */
     void setScale(int s);
+    /**
+     * @brief Set the Pos of the text
+     * 
+     * @param x 
+     * @param y 
+     */
     void setPos(int x, int y);
+    /**
+     * @brief Set the Padding for the text
+     * 
+     * @param val 
+     */
     void setPadding(int val);
+    /**
+     * @brief Set the Value of the text
+     * 
+     * @param data 
+     */
     void setValue(String data);
     void calculatePadding();
     void calculatePosition();
+    /**
+     * @brief Draw line going trough the center of the text
+     * 
+     */
     void centerCut();
+    /**
+     * @brief Underline the text
+     * 
+     */
     void underline();
+    /**
+     * @brief Set the left border visible 
+     * 
+     * @param val 
+     */
     void setBorderLeft(bool val);
+    /**
+     * @brief Set the right border visible
+     * 
+     * @param val 
+     */
     void setBorderRight(bool val);
+    /**
+     * @brief Set the top border visible
+     * 
+     * @param val 
+     */
     void setBorderTop(bool val);
+    /**
+     * @brief Set the bottom border visible
+     * 
+     * @param val 
+     */
     void setBorderBottom(bool val);
+    /**
+     * @brief Set the Border visible
+     * 
+     * @param val 
+     */
     void setBorder(bool val);
+    /**
+     * @brief Draw the text
+     * 
+     * @param oled 
+     */
     void draw(OledHelper &oled);
 };
 
